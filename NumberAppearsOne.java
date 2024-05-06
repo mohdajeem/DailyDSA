e2a9f8a7b86b02b74859505e82a9f77a77cf4df1
mohdajeem <149220624+mohdajeem@users.noreply.github.com>
2024-05-06 22:38:46 +0530

    with optimal approach

--- NumberAppearsOne.java
+++ NumberAppearsOne.java
@@ -1,24 +1,31 @@
 public class NumberAppearsOne {
 
     //brute force approach
-    public static int count(int[] arr){
+    public static int countLeast(int[] arr){
         int count;
         for(int i = 0;i<arr.length;i++){
             count = 0;
-            for(int j = i+1;j<arr.length;j++){
+            for(int j = 0;j<arr.length;j++){
                 if(arr[i]==arr[j]){
                     count++;
                 }
             }
-            if(count == 0){
+            if(count == 1){
                 return arr[i];
             }
         }
         return -1;
     }
-    public static void main(String[] args){
-        int[] arr = {1,2,3,4,3,2,1};
-        System.out.println(count(arr));
-
+    public static int xorCount(int[] arr){
+        int xor = 0;
+        for(int i = 0;i<arr.length;i++){
+            xor = xor^arr[i];
+        }
+        return xor;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 3, 2, 1,4,5,11,5};
+        System.out.println(countLeast(arr));
+        System.out.println(xorCount(arr));
     }
 }
